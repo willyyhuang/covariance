0f88fab99722034ce5b0da556d45bb3bc1e11101
Wilson <wilsonhuang00815@gmail.com>
2021-04-18 20:28:58 +0800

    add ui library and sample page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Avatar,
+  Card,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
+const App = () => {
+  const projects = [
+    {
+      name: 'Project Name 1',
+      detail:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+    },
+    {
+      name: 'Project Name 2',
+      detail:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+    },
+  ]
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card style={{margin: '20vh 20vw 20vh 20vw'}}>
+      <Grid container style={{margin: '16px 0px 16px 0px'}}>
+        <Grid item xs={1} md={1} lg={1} />
+        <Grid item xs={2} md={2} lg={2}>
+          <Avatar />
+        </Grid>
+        <Grid item xs={8} md={8} lg={8}>
+          {projects.map((value) => (
+            <Accordion>
+              <AccordionSummary>
+                <Typography>{value.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{value.detail}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Grid>
+        <Grid item xs={1} md={1} lg={1} />
+      </Grid>
+    </Card>
   )
 }
 
