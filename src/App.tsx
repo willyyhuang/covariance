f1758bbfde4e2b7cd053d1397d42680a0c99f23e
Wilson <wilsonhuang00815@gmail.com>
2021-04-19 22:02:00 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,10 @@
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Avatar,
-  Card,
-  Grid,
-  Typography,
-} from '@material-ui/core'
+import {BrowserRouter, Route} from 'react-router-dom'
 import React from 'react'
+import {Overview} from './views'
 
-const App = () => {
-  const projects = [
-    {
-      name: 'Project Name 1',
-      detail:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
-    },
-    {
-      name: 'Project Name 2',
-      detail:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
-    },
-  ]
-
-  return (
-    <Card style={{margin: '20vh 20vw 20vh 20vw'}}>
-      <Grid container style={{margin: '16px 0px 16px 0px'}}>
-        <Grid item xs={1} md={1} lg={1} />
-        <Grid item xs={2} md={2} lg={2}>
-          <Avatar />
-        </Grid>
-        <Grid item xs={8} md={8} lg={8}>
-          {projects.map((value) => (
-            <Accordion style={{margin: '16px 0px 16px 0px'}}>
-              <AccordionSummary>
-                <Typography>{value.name}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>{value.detail}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          ))}
-        </Grid>
-        <Grid item xs={1} md={1} lg={1} />
-      </Grid>
-    </Card>
-  )
-}
-
+const App = () => (
+  <BrowserRouter>
+    <Route path='/' component={Overview} />
+  </BrowserRouter>
+)
 export default App
---
+++ src/views/Overview/Overview.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Avatar,
+  Card,
+  Grid,
+  Typography,
+} from '@material-ui/core'
+
+const OverView = () => {
+  const projects = [
+    {
+      name: 'Project Name 1',
+      detail:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+    },
+    {
+      name: 'Project Name 2',
+      detail:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+    },
+  ]
+
+  return (
+    <Card style={{margin: '20vh 20vw 20vh 20vw'}}>
+      <Grid container style={{margin: '16px 0px 16px 0px'}}>
+        <Grid item xs={1} md={1} lg={1} />
+        <Grid item xs={2} md={2} lg={2}>
+          <Avatar />
+        </Grid>
+        <Grid item xs={8} md={8} lg={8}>
+          {projects.map((value) => (
+            <Accordion style={{margin: '16px 0px 16px 0px'}}>
+              <AccordionSummary>
+                <Typography>{value.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{value.detail}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Grid>
+        <Grid item xs={1} md={1} lg={1} />
+      </Grid>
+    </Card>
+  )
+}
+
+OverView.displayName = 'OverView'
+export default OverView
