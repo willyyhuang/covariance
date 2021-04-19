a6c66c35ec7b1b32516b23c2e681e0badca9e61d
Wilson <wilsonhuang00815@gmail.com>
2021-04-19 22:22:49 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Redirect} from 'react-router-dom'
 import React from 'react'
 import {Overview} from './views'
 
 const App = () => (
   <BrowserRouter>
-    <Route path='/' component={Overview} />
+    <Route path='*'>
+      <Redirect to='/' />
+    </Route>
+    <Route exact path='/' component={Overview} />
   </BrowserRouter>
 )
 export default App
