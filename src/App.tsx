43713ba33fa7ce5a188d6ecd4f0b74eb5d143d39
Wilson <wilsonhuang00815@gmail.com>
2021-04-21 21:56:22 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import {BrowserRouter, Route, Redirect} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import React from 'react'
 import {Overview, ProjectDetail} from './views'
 
 const App = () => (
   <BrowserRouter>
-    <Route path='/soen-357'>
-      <Redirect to='/' />
-    </Route>
     <Route path='/:id' component={ProjectDetail} />
     <Route exact path='/' component={Overview} />
   </BrowserRouter>
