e27719f80fb5edf9936a2f9c9bb8c03d28edacf4
Wilson <wilsonhuang00815@gmail.com>
2021-04-21 22:30:36 +0800

    use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import React from 'react'
 import {Overview, ProjectDetail} from './views'
 
 const App = () => (
   <Router basename={process.env.PUBLIC_URL}>
     <Switch>
-      <Route path='/:id' component={ProjectDetail} />
-      <Route path='/' component={Overview} />
+      <Route exact path='/' component={Overview} />
+      <Route path='/Project/:id' component={ProjectDetail} />
     </Switch>
   </Router>
 )
