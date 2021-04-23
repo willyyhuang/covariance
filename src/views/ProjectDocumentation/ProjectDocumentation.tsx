3bc9948c6a13ed68be6930e42792f870d4d23706
Wilson <wilsonhuang00815@gmail.com>
2021-04-23 14:04:57 +0800

    update

--- src/views/ProjectDocumentation/ProjectDocumentation.tsx
+++ src/views/ProjectDocumentation/ProjectDocumentation.tsx
@@ -1,39 +1,55 @@
 import React from 'react'
-import {Box, Button, Card, CardContent, CardHeader, IconButton, List, Typography} from '@material-ui/core'
-// import {Edit} from '@material-ui/icons'
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  IconButton,
+  Typography,
+} from '@material-ui/core'
 import {AddCircleOutline, ArrowBack} from '@material-ui/icons'
-import {BrowserRouter} from 'react-router-dom'
 import projectDocuments from '../../mock/projectDocumentation.json'
 
-
-type ProjectDocumentationPropType = {}
-
-const ProjectDocumentation = (props: ProjectDocumentationPropType) => {
-  console.log(props)
-  return (
-      <List>
-          <Box display='flex'>
-              <Button onClick={BrowserRouter.goBack} style={{margin: '2vw 2vh 2vw 2vh'}} variant='outlined' color='primary'>
-                  <ArrowBack />
-                  Back to Project
-              </Button>
-              <IconButton style={{margin: '0vw 0vh 0vw 90vh'}}>
-                <AddCircleOutline fontSize='large' />
-              </IconButton>
-          </Box>
-          {projectDocuments.documents.map((items) => (
-                  <Card style={{margin: '2vw 2vh 2vw 2vh'}}>
-                      <CardHeader title={items.title} action='Edit' />
-                      <CardContent style={{margin: '0vw 2vh 0vw 2vh'}}>
-                          <Typography variant='body1' style={{whiteSpace: 'pre-line'}}>
-                              {items.body}
-                          </Typography>
-                      </CardContent>
-                  </Card>
-          ))}
-      </List>
-  )
-}
+const ProjectDocumentation = () => (
+  <Card style={{margin: '20vh 20vw 20vh 20vw'}}>
+    <Grid container style={{margin: '16px 0px 16px 0px'}}>
+      <Grid item xs={4} md={4} lg={4}>
+        <Button
+          style={{margin: '14px 0px 0px 16px'}}
+          href='#/Project/1'
+          variant='outlined'
+          color='primary'>
+          <ArrowBack />
+          Back to Project
+        </Button>
+      </Grid>
+      <Grid item xs={7} md={7} lg={7} />
+      <Grid item xs={1} md={1} lg={1}>
+        <IconButton>
+          <AddCircleOutline fontSize='large' />
+        </IconButton>
+      </Grid>
+    </Grid>
+    {projectDocuments.documents.map((items) => (
+      <Card style={{padding: 16, margin: '16px 0px 16px 0px'}}>
+        <CardHeader
+          title={items.title}
+          action={
+            <Typography style={{color: '#3366bb', marginTop: 8}}>
+              Edit
+            </Typography>
+          }
+        />
+        <CardContent>
+          <Typography variant='body1' style={{whiteSpace: 'pre-line'}}>
+            {items.body}
+          </Typography>
+        </CardContent>
+      </Card>
+    ))}
+  </Card>
+)
 
 ProjectDocumentation.displayName = 'ProjectDocumentation'
 export default ProjectDocumentation
